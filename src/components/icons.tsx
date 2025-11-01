4d41f0cf07363665d4026577f62928298312e3d6
MAA KALKA HARDWARE & PAINTS <maakalkahardwarepaints@gmail.com>
2025-11-01 13:39:00 +0000

    log is not clear

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -10,29 +10,8 @@ export function JMKTradingLogo(props: SVGProps<SVGSVGElement>) {
       {...props}
     >
       <path
-        fill="#004d61"
-        d="M232,216a8,8,0,0,1-8,8H32a8,8,0,0,1-6.4-12.8l96-128a8,8,0,0,1,12.8,0l96,128A8,8,0,0,1,232,216Z"
-        transform="scale(1, 0.2) translate(0, 830)"
-      />
-      <path
         fill="#007a99"
-        d="M24,72v128h88V48.15A7.94,7.94,0,0,0,105.6,40L24,56Z"
-        transform="translate(0, -10)"
-      />
-      <path
-        fill="#007a99"
-        d="M232,72v128h-88V48.15a7.94,7.94,0,0,1,6.4-7.85L232,56Z"
-        transform="translate(0, -10)"
-      />
-      <path
-        fill="#00c2e0"
-        d="M112,48.15V190H32V64.21Z"
-        transform="translate(0, -10)"
-      />
-      <path
-        fill="#00c2e0"
-        d="M144,48.15V190h80V64.21Z"
-        transform="translate(0, -10)"
+        d="M216 40H40a16 16 0 0 0-16 16v144a16 16 0 0 0 16 16h176a16 16 0 0 0 16-16V56a16 16 0 0 0-16-16Zm0 160H40V56h80v144h96Z"
       />
     </svg>
   );
