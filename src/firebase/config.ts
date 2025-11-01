bac9751848f4a14fcf2a09428118bcce7871b084
MAA KALKA HARDWARE & PAINTS <maakalkahardwarepaints@gmail.com>
2025-11-01 12:51:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5571152152-a8763",
+  "appId": "1:333974946501:web:169faa407ac8b2975be6ac",
+  "apiKey": "AIzaSyABz4mByLP-R8YGaD-IS0HD0rYbazFKNLA",
+  "authDomain": "studio-5571152152-a8763.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "333974946501"
+};
